14dfad5049c6689c05aff37cd4221a423969f4a0
박정훈 <ggongjja0@naver.com>
2024-02-26 15:49:35 +0900

    create components dir, MainPage.jsx

---
+++ src/components/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MainPage = () => {
+  return (
+    <header className="App-header">
+      <h1>
+        <div>Hello, My name is</div>
+        <div>JeongHoon Park</div>
+      </h1>
+    </header>
+  );
+};
+
+export default MainPage;
