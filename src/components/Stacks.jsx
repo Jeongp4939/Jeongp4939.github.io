9f5a842f7fb5e3a606177a3414f6c89ed0e342ff
박정훈 <ggongjja0@naver.com>
2024-06-09 19:14:10 +0900

    feat: contact, stacks 추가

---
+++ src/components/Stacks.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Stacks = () => {
+  return (
+    <header className="App-header">
+      <h1>
+        <div>My Projects</div>
+      </h1>
+    </header>
+  );
+};
+
+export default Stacks;
