0bb30c1daaae6919798eb3b078eec2e6a0350fc3
박정훈 <ggongjja0@naver.com>
2024-08-17 18:39:20 +0900

    mod: Stacks skill -> lang 변경

--- src/components/Stacks.jsx
+++ src/components/Stacks.jsx
@@ -2,38 +2,33 @@ import React from "react";
 import "../styles/stacks.css";
 
 const Stacks = () => {
-  const skills = [
+  const lang = [
     {
-      skill: "Python",
+      name: "Python",
       periodOfUse: "2년",
       useCase: ["Django 백앤드 CRUD 구성", "OpenCV 기반 문자인식 기능 구현"],
     },
     {
-      skill: "JavaScript",
+      name: "JavaScript",
       periodOfUse: "2년",
       useCase: ["React 이용 UI 생성 및 API 연동", "타이머 기능 구현"],
-    },
-    {
-      skill: "",
-      periodOfUse: "",
-      useCase: [""],
     },
   ];
 
   return (
     <header className="App-header">
-      <h2>기술 스택</h2>
+      <h2>언어</h2>
       <div>
-        {skills.map((skill, idx) => {
+        {lang.map((lang, idx) => {
           return (
             <div>
               <div>
-                {idx +1}. {skill.skill}
+                {idx +1}. {lang.name}
               </div>
-              <div>사용 기간 : {skill.periodOfUse}</div>
+              <div>사용 기간 : {lang.periodOfUse}</div>
               <div>
                 <div>경험</div>
-                {skill.useCase.map((usecase, idx) => {
+                {lang.useCase.map((usecase, idx) => {
                   return (
                     <div>
                       {idx + 1}. {usecase}
